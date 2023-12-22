06334c41f57d3af8cb39bae8e2c9a2837778f225
luisquix <120378585+luisquix@users.noreply.github.com>
2023-12-22 10:28:05 +0100

    Add initial install scripts

---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+# inherited from https://github.com/release-lab/install
+
+set -e
+
+get_arch() {
+    # darwin/amd64: Darwin axetroydeMacBook-Air.local 20.5.0 Darwin Kernel Version 20.5.0: Sat May  8 05:10:33 PDT 2021; root:xnu-7195.121.3~9/RELEASE_X86_64 x86_64
+    # linux/amd64: Linux test-ubuntu1804 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux
+    a=$(uname -m)
+    case ${a} in
+        "x86_64" | "amd64" )
+            echo "x64"
+        ;;
+        "aarch64" | "arm64" | "arm")
+            echo "arm64"
+        ;;        
+        *)
+            echo ${NIL}
+        ;;
+    esac
+}
+
+get_os(){
+    # darwin: Darwin
+    echo $(uname -s | awk '{print tolower($0)}')
+}
+
+# Parse arguments
+for i in "$@"; do
+    case $i in
+        -v=*|--version=*)
+            version="${i#*=}"
+            shift # past argument=value
+        ;;
+        *)
+            # unknown option
+        ;;
+    esac
+done
+
+owner="quixio"
+repo="quix-cli"
+githubUrl="https://github.com"
+exe_name="quix"
+
+downloadFolder="${TMPDIR:-/tmp}"
+mkdir -p ${downloadFolder} # make sure download folder exists
+os=$(get_os)
+arch=$(get_arch)
+file_name="${os}-${arch}.zip" # the file name should be download
+downloaded_file="${downloadFolder}/${file_name}" # the file path should be download
+executable_folder="/usr/local/bin" # Eventually, the executable file will be placed here
+
+# if version is empty
+if [ -z "$version" ]; then
+    asset_uri="${githubUrl}/${owner}/${repo}/releases/latest/download/${file_name}"
+else
+    asset_uri="${githubUrl}/${owner}/${repo}/releases/download/${version}/${file_name}"
+fi
+
+echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
+rm -f ${downloaded_file}
+curl --fail --location --output "${downloaded_file}" "${asset_uri}"
+
+echo "[2/3] Install ${exe_name} to the ${executable_folder}"
+echo  -o "${downloaded_file}" -d "${executable_folder}"
+unzip -o "${downloaded_file}" -d "${executable_folder}"
+
+exe="${executable_folder}/${exe_name}"
+chmod +x "${exe}"
+
+echo "[3/3] Set environment variables"
+echo "${exe_name} was installed successfully to ${exe}"
+if command -v $exe_name --version >/dev/null; then
+    echo "Run '$exe_name --help' to get started"
+else
+    echo "Manually add the directory to your \$HOME/.bash_profile (or similar)"
+    echo "  export PATH=${executable_folder}:\$PATH"
+    echo "Run '$exe_name --help' to get started"
+fi
+
+exit 0
