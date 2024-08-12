b900df7a1f4c4ecfde605f99c5ed7db01304a49f
Ricardo Kissinger <ricardo.kissinger@juniz.com>
2024-08-12 14:14:40 +0200

    Add quix-cli formula

--- quix-cli.rb
+++ quix-cli.rb
@@ -6,22 +6,18 @@ class QuixCli < Formula
     sha256 "216c5888bacef04f9af38a209616004ddde41c5c50a5507dfff75a6279412780"
   
     def install
-        # Specify the local installation directory
-        local_bin = "#{ENV['HOME']}/.local/bin"
-        system "mkdir", "-p", local_bin
-        system "bash", "install.sh", "--", "--prefix=#{local_bin}"
+        # Use the prefix provided by Homebrew
+        system "bash", "install.sh", "--prefix=#{prefix}"
       end
     
       def caveats
         <<~EOS
-          Quix CLI has been installed in your local bin directory: ~/.local/bin.
-          To use it, you may need to add the following to your shell configuration:
-          
-          export PATH="$HOME/.local/bin:$PATH"
+          Quix CLI has been installed in #{HOMEBREW_PREFIX}/bin.
+          You can run it with the command 'quix'.
         EOS
       end
     
       test do
-        system "#{ENV['HOME']}/.local/bin/quix", "--version"
+        system "#{bin}/quix", "--version"
       end
     end
