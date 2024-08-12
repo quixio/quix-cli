e0d94565cea44b846baa588807c286cd8a673221
Ricardo Kissinger <ricardo.kissinger@juniz.com>
2024-08-12 14:06:44 +0200

    Add quix-cli formula

--- quix-cli.rb
+++ quix-cli.rb
@@ -6,10 +6,22 @@ class QuixCli < Formula
     sha256 "216c5888bacef04f9af38a209616004ddde41c5c50a5507dfff75a6279412780"
   
     def install
-      system "bash", "install.sh", "--", prefix
+        # Specify the local installation directory
+        local_bin = "#{ENV['HOME']}/.local/bin"
+        system "mkdir", "-p", local_bin
+        system "bash", "install.sh", "--", "--prefix=#{local_bin}"
+      end
+    
+      def caveats
+        <<~EOS
+          Quix CLI has been installed in your local bin directory: ~/.local/bin.
+          To use it, you may need to add the following to your shell configuration:
+          
+          export PATH="$HOME/.local/bin:$PATH"
+        EOS
+      end
+    
+      test do
+        system "#{ENV['HOME']}/.local/bin/quix", "--version"
+      end
     end
-  
-    test do
-      system "#{bin}/quix", "--version"
-    end
-  end
