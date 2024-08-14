8266c6e8945131b3c04f678a3d0c9f3b993dc498
Ricardo Kissinger <ricardo.kissinger@juniz.com>
2024-08-14 17:42:39 +0200

    add updated homebrew file

--- quix-cli.rb
+++ quix-cli.rb
@@ -1,23 +1,34 @@
 class QuixCli < Formula
-    desc "Quix CLI tool"
+    desc "Quix CLI"
     homepage "https://github.com/quixio/quix-cli"
-    url "https://github.com/spkis/quix-cli/raw/main/install.sh"
-    version "0.1.0"
-    sha256 "216c5888bacef04f9af38a209616004ddde41c5c50a5507dfff75a6279412780"
+    version "latest" # Or specify a specific version if desired
+    license "MIT"
+  
+    # Define the URLs for the binaries based on architecture
+    if Hardware::CPU.intel?
+      url "https://github.com/quixio/quix-cli/releases/latest/download/osx-x64.tar.gz"
+      sha256 "b966d3db258866183ddd9002ca910ddbd9af7460c82f3fb7c2e057ae96fc5ffb"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/quixio/quix-cli/releases/latest/download/osx-arm64.tar.gz"
+      sha256 "60739eb440654b226ec5ccfb1da953693d8a8ee5e1a71b32ed1f717c6e82709e"
+    else
+      odie "Unsupported architecture"
+    end
   
     def install
-        # Use the prefix provided by Homebrew
-        system "bash", "install.sh", "--prefix=#{HOMEBREW_PREFIX}"
-      end
-    
-      def caveats
-        <<~EOS
-          Quix CLI has been installed in #{HOMEBREW_PREFIX}/bin.
-          You can run it with the command 'quix'.
-        EOS
-      end
-    
-      test do
-        system "#{bin}/quix", "--version"
+      bin.install "quix" # Installs the binary into the Homebrew bin folder
+    end
+  
+    def post_install
+      # Any post-installation actions like signing the binary
+      if OS.mac?
+        system "codesign", "-s", "-", "#{bin}/quix"
       end
     end
+  
+    test do
+      # Basic test to verify installation
+      system "#{bin}/quix", "--version"
+    end
+  end
+  
